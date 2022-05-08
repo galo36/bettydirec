4e609956747369deeba339038a59f7c68cb63aa1
galo36 <99937579+galo36@users.noreply.github.com>
2022-05-08 02:52:09 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,5 @@
+api_hash = "f69ab9f221ce44a4f0541e6f4624f431"
+api_id = 16923599
+bot_token = "5396349304:AAGPlMM3MppEY4mYEKl5GMIFaVebXcviAeQ"
+authorized_users = ['PILOTS21','ELDIOSMESSI']
+admin_user = 'DAMIANBZM'
