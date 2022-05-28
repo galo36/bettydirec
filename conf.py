b285311480728234a14d1793ee4638a662b4dcc2
galo36 <99937579+galo36@users.noreply.github.com>
2022-05-28 00:07:35 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "f69ab9f221ce44a4f0541e6f4624f431"
 api_id = 16923599
-bot_token = "5338746826:AAEgK-7z-ntduzgU__v2s17XnjKrSE9o6gA"
+bot_token = "5550496407:AAGrnABVSgN80CI4e7gRWntnkW9pmSajYpA"
 authorized_users = ['PILOTS21','ELDIOSMESSI']
 admin_user = 'DAMIANBZM'
