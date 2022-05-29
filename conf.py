3a773cc4b733b39d5d007cfeb031eb29a11c0c48
galo36 <99937579+galo36@users.noreply.github.com>
2022-05-29 02:32:56 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,6 @@
-api_hash = "f69ab9f221ce44a4f0541e6f4624f431"
-api_id = 16923599
-bot_token = "5550496407:AAGrnABVSgN80CI4e7gRWntnkW9pmSajYpA"
-authorized_users = ['PILOTS21','ELDIOSMESSI']
-admin_user = 'DAMIANBZM'
+api_hash = "1d687eefd91f2d5154dd089423eeb858"
+api_id = 9879198
+bot_token = "
+5529540116:AAGPD2pC6RdlyhtuhQfEURzKDKqFF-Vs6hY"
+authorized_users = ['Ami_801','Nueva_Carpeta','Pecas05','Magares','S4S4N1M','lacanty','duduncia','blonde_eyebrow']
+admin_user = 'BettyB96'
