664124641f441ba1feb2b00856f4afa6f0ae9a5e
galo36 <99937579+galo36@users.noreply.github.com>
2022-05-29 02:33:48 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,5 @@
 api_hash = "1d687eefd91f2d5154dd089423eeb858"
 api_id = 9879198
-bot_token = "
-5529540116:AAGPD2pC6RdlyhtuhQfEURzKDKqFF-Vs6hY"
+bot_token = "5529540116:AAGPD2pC6RdlyhtuhQfEURzKDKqFF-Vs6hY"
 authorized_users = ['Ami_801','Nueva_Carpeta','Pecas05','Magares','S4S4N1M','lacanty','duduncia','blonde_eyebrow']
 admin_user = 'BettyB96'
